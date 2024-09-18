574fc20b74acae04d7c22acc6d6f93be8ae2ea31
NairDeepesh <deepeshnair05@gmail.com>
2024-09-18 12:26:46 +0530

    Second commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ## MLFLOW PROJECT
+
+import dagshub
+dagshub.init(repo_owner='NairDeepesh', repo_name='MLFlowProject', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
--- app.py
+++ app.py
@@ -17,6 +17,9 @@ import mlflow
 import mlflow.sklearn
 from mlflow.models import infer_signature
 
+import dagshub
+dagshub.init(repo_owner='NairDeepesh', repo_name='MLFlowProject', mlflow=True)
+
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
@@ -55,6 +58,8 @@ if __name__ == "__main__":
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
+
+
     with mlflow.start_run():
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
@@ -76,6 +81,10 @@ if __name__ == "__main__":
 
         predictions = lr.predict(train_x)
         signature = infer_signature(train_x, predictions)
+
+        ## For Remote Server only (Dagshub)
+        remote_server_uri="https://dagshub.com/NairDeepesh/MLFlowProject.mlflow"
+        mlflow.set_tracking_uri(remote_server_uri)
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
